c74049686323523f54cc4b44cabe1855fcaa3cc9
Diogo Campos <d.diogocampos@gmail.com>
2017-05-21 17:37:57 +0100

    Updated launch scripts

--- scripti.sh
+++ scripti.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export PATH=$PATH:/usr/local/go/bin
 python3 shuffle.py --ignore
-go build
-./hello ignore
+go run polishBankrupt.go
--- scripts.sh
+++ scripts.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export PATH=$PATH:/usr/local/go/bin
 python3 shuffle.py
-go build
-./hello
+go run polishBankrupt.go
